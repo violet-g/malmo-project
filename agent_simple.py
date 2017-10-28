e5b6d0a03e0705c7a3c8406f137323d94cf85b04
violet-g <gabriella.georgieva17@gmail.com>
2017-10-28 18:54:50 +0100

    Extracted simple agent into a separate file

---
+++ agent_simple.py
@@ -0,0 +1,31 @@
+#--------------------------------------------------------------------------------------
+#-- This class implements the Simple Agent --#
+class AgentSimple:
+
+    def __init__(self,agent_host,agent_port, mission_type, mission_seed, solution_report, state_space):
+        """ Constructor for the simple agent """
+        self.AGENT_MOVEMENT_TYPE = 'Discrete' # HINT: You can change this if you want {Absolute, Discrete, Continuous}
+        self.AGENT_NAME = 'Simple'
+
+        self.agent_host = agent_host
+        self.agent_port = agent_port
+        self.mission_seed = mission_seed
+        self.mission_type = mission_type
+        self.state_space = state_space;
+        self.solution_report = solution_report;  # Python calls by reference !
+        self.solution_report.setMissionType(self.mission_type)
+        self.solution_report.setMissionSeed(self.mission_seed)
+
+    def run_agent(self):
+        """ Run the Simple agent and log the performance and resource use """
+
+        #-- Load and init mission --#
+        print('Generate and load the ' + self.mission_type + ' mission with seed ' + str(self.mission_seed) + ' allowing ' +  self.AGENT_MOVEMENT_TYPE + ' movements')
+        mission_xml = init_mission(self.agent_host, self.agent_port, self.AGENT_NAME, self.mission_type, self.mission_seed, self.AGENT_MOVEMENT_TYPE)
+        self.solution_report.setMissionXML(mission_xml)
+        time.sleep(1)
+        self.solution_report.start()
+
+        # INSERT: YOUR SOLUTION HERE (REMEMBER TO MANUALLY UPDATE THE solution_report DEPENDING ON YOU SOLUTION)
+
+        return
--- myagents.py
+++ myagents.py
@@ -293,38 +293,6 @@ def init_mission(agent_host, port=0, agent_type='Unknown',mission_type='Unknown'
     return final_xml,reward_goal,reward_intermediate,n_intermediate_rewards,reward_timeout,reward_sendcommand,timeout
 
 #--------------------------------------------------------------------------------------
-#-- This class implements the Simple Agent --#
-class AgentSimple:
-
-    def __init__(self,agent_host,agent_port, mission_type, mission_seed, solution_report, state_space):
-        """ Constructor for the simple agent """
-        self.AGENT_MOVEMENT_TYPE = 'Discrete' # HINT: You can change this if you want {Absolute, Discrete, Continuous}
-        self.AGENT_NAME = 'Simple'
-
-        self.agent_host = agent_host
-        self.agent_port = agent_port
-        self.mission_seed = mission_seed
-        self.mission_type = mission_type
-        self.state_space = state_space;
-        self.solution_report = solution_report;  # Python calls by reference !
-        self.solution_report.setMissionType(self.mission_type)
-        self.solution_report.setMissionSeed(self.mission_seed)
-
-    def run_agent(self):
-        """ Run the Simple agent and log the performance and resource use """
-
-        #-- Load and init mission --#
-        print('Generate and load the ' + self.mission_type + ' mission with seed ' + str(self.mission_seed) + ' allowing ' +  self.AGENT_MOVEMENT_TYPE + ' movements')
-        mission_xml = init_mission(self.agent_host, self.agent_port, self.AGENT_NAME, self.mission_type, self.mission_seed, self.AGENT_MOVEMENT_TYPE)
-        self.solution_report.setMissionXML(mission_xml)
-        time.sleep(1)
-        self.solution_report.start()
-
-        # INSERT: YOUR SOLUTION HERE (REMEMBER TO MANUALLY UPDATE THE solution_report DEPENDING ON YOU SOLUTION)
-
-        return
-
-#--------------------------------------------------------------------------------------
 #-- This class implements a basic, suboptimal Random Agent. The purpurpose is to provide a baseline for other agent to beat. --#
 class AgentRandom:
 
@@ -367,7 +335,7 @@ class AgentRandom:
         self.agent_host.setRewardsPolicy(MalmoPython.RewardsPolicy.KEEP_ALL_REWARDS)
 
         # Fix the randomness of the agent by seeding the random number generator
-        reward_cumulative = 0.0
+        reward_cumulative = 0.3
 
         # Main loop:
         state_t = self.agent_host.getWorldState()
