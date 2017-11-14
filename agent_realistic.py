7e2c3252509de6bce5cb6effcad5ee47259a7898
Philippe <ppartarr@outlook.com>
2017-11-14 18:10:28 +0000

    wip: agent realistic from scratch

--- agent_realistic.py
+++ agent_realistic.py
@@ -24,7 +24,6 @@ class AgentRealistic:
         self.solution_report = solution_report;   # Python is call by reference !
         self.solution_report.setMissionType(self.mission_type)
         self.solution_report.setMissionSeed(self.mission_seed)
-        self.actions = []
 
     #----------------------------------------------------------------------------------------------------------------#
     def __ExecuteActionForRealisticAgentWithNoisyTransitionModel__(idx_requested_action, noise_level):
@@ -36,44 +35,6 @@ class AgentRealistic:
         actual_action = self.AGENT_ALLOWED_ACTIONS[int(idx_actual)]
         self.agent_host.sendCommand(actual_action)
         return actual_action
-
-    def act_agent(self, world_state, agent_host, reward_cumulative):
-        """ Take an action in response to the current world state"""
-        
-        obs_text = world_state.observations[-1].text
-        obs = json.loads(obs_text) # most recent observation
-        self.logger.debug(obs)
-        if not u'XPos' in obs or not u'ZPos' in obs:
-            self.logger.error("Incomplete observation received: %s" % obs_text)
-            return 0
-        current_s = "%d:%d" % (int(obs[u'XPos']), int(obs[u'ZPos']))
-        self.logger.debug("State: %s (x = %.2f, z = %.2f)" % (current_s, float(obs[u'XPos']), float(obs[u'ZPos'])))
-        if not self.q_table.has_key(current_s):
-            self.q_table[current_s] = ([0] * len(self.actions))
-
-        # update Q values
-        if self.training and self.prev_s is not None and self.prev_a is not None:
-            old_q = self.q_table[self.prev_s][self.prev_a]
-            self.q_table[self.prev_s][self.prev_a] = old_q + self.alpha * (reward_cumulative
-                + self.gamma * max(self.q_table[current_s]) - old_q)
-
-        # select the next action
-        m = max(self.q_table[current_s])
-        self.logger.debug("Current values: %s" % ",".join(str(x) for x in self.q_table[current_s]))
-        l = list()
-        for x in range(0, len(self.actions)):
-            if self.q_table[current_s][x] == m:
-                l.append(x)
-        y = random.randint(0, len(l)-1)
-        a = l[y]
-        self.logger.info("Taking q action: %s" % self.actions[a])
-
-        # send the selected action
-        agent_host.ExecuteActionForRealisticAgentWithNoisyTransitionModel(self.actions[a], 0.0) # TODO add noise for submission
-        self.prev_s = current_s
-        self.prev_a = a
-
-        return reward_cumulative
 
     #----------------------------------------------------------------------------------------------------------------#
     def run_agent(self):
@@ -92,14 +53,11 @@ class AgentRealistic:
         #       ExecuteActionForRealisticAgentWithNoisyTransitionModel(idx_requested_action, 0.05)
         #   FOR DEVELOPMENT IT IS RECOMMENDED TO FIST USE A NOISE FREE VERSION, i.e.
         #       ExecuteActionForRealisticAgentWithNoisyTransitionModel(idx_requested_action, 0.0)
-		
-        # Wait for a valid observation
-        state_space_locations = self.state_space.state_locations
-        print(state_space_locations)
 
         continuousMovement = False
 		
         self.agent_host.setObservationsPolicy(MalmoPython.ObservationsPolicy.LATEST_OBSERVATION_ONLY)
+        self.agent_host.setRewardsPolicy(MalmoPython.RewardsPolicy.SUM_REWARDS)
         self.agent_host.setVideoPolicy(MalmoPython.VideoPolicy.LATEST_FRAME_ONLY)
 
         # Goal:
@@ -108,42 +66,36 @@ class AgentRealistic:
         reward_cumulative = 0.0
         total_reward = 0
 
-        self.prev_s = None
-        self.prev_a = None
-
-        # wait for a valid observation
-        world_state = agent_host.peekWorldState()
-        while world_state.is_mission_running and all(e.text=='{}' for e in world_state.observations):
-            world_state = agent_host.peekWorldState()
-        # wait for a frame to arrive after that
-        num_frames_seen = world_state.number_of_video_frames_since_last_state
-        while world_state.is_mission_running and world_state.number_of_video_frames_since_last_state == num_frames_seen:
-            world_state = agent_host.peekWorldState()
-        world_state = agent_host.getWorldState()
-        for err in world_state.errors:
-            print err
-
-        assert len(world_state.video_frames) > 0, 'No video frames!?'
-        
-        obs = json.loads( world_state.observations[-1].text )
-        prev_x = obs[u'XPos']
-        prev_z = obs[u'ZPos']
-        print 'Initial position:',prev_x,',',prev_z
-            
-        # take first action
-        total_reward += ExecuteActionForRealisticAgentWithNoisyTransitionModel(, 0.0)
-        total_reward += self.act(world_state,agent_host, reward_cumulative)
-        
-        require_move = True
-        check_expected_position = True
-
-		#get state_t 
+        """
         state_t = self.agent_host.getWorldState()
+
+        if state_t.number_of_observations_since_last_state > 0: # Has any Oracle-like and/or internal sensor observations come in?
+            msg = state_t.observations[-1].text      # Get the detailed for the last observed state
+            oracle = json.loads(msg)                 # Parse the Oracle JSON
+
+            # Oracle
+            grid = oracle.get(u'grid', 0)
+
+            # GPS-like sensor
+            xpos = oracle.get(u'XPos', 0)            # Position in 2D plane, 1st axis
+            zpos = oracle.get(u'ZPos', 0)            # Position in 2D plane, 2nd axis (yes Z!)
+            ypos = oracle.get(u'YPos', 0)
+
+
+
+        q_table = [][]
+        reward_matrix = [][]
+        gamma = 0                                   # Greediness of the agent. Closer to 0 is greedier
+        """
+        #get state_t 
+        state_t = self.agent_host.getWorldState()
+
+        print state_t.observations
+
+
 
 		#Main Loop
         while state_t.is_mission_running:
-
-        ### ADD STUFF IN MAIN LOOP
 
 			#Set the world state
             state_t = self.agent_host.getWorldState()
@@ -166,5 +118,5 @@ class AgentRealistic:
                     actionIdx = random.randint(0, 2)
                     self.agent_host.sendCommand(discreteAction[actionIdx])
 
-			#
+			
         return
