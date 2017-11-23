498d0067520edb1521a5b28c6c77d76162a94039
violet-g <gabriella.georgieva17@gmail.com>
2017-11-23 21:45:46 +0000

    Optimised agent realistic

--- agent_realistic.py
+++ agent_realistic.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 import random
 import json
 import numpy as np
@@ -42,10 +43,10 @@ class AgentRealistic:
         """ Run the Realistic agent and log the performance and resource use """
 
         try:
-            Q = pickle.load(open("Q.txt", "rb"))
+            Q = pickle.load(open("Q.pickle", "rb"))
         except (OSError, IOError) as e:
             print "Creating Q ..."
-            Q = [[0] * 4 for i in range(100)]
+            Q = [[100] * 4 for i in range(100)]
 
         #-- Load and init mission --#
         print('Generate and load the ' + self.mission_type + ' mission with seed ' + str(self.mission_seed) + ' allowing ' +  self.AGENT_MOVEMENT_TYPE + ' movements')
@@ -66,37 +67,12 @@ class AgentRealistic:
         self.agent_host.setVideoPolicy(MalmoPython.VideoPolicy.LATEST_FRAME_ONLY)
         self.agent_host.setRewardsPolicy(MalmoPython.RewardsPolicy.KEEP_ALL_REWARDS)
 
-        BLOCK_TYPES = {"stone":-1000, "glowstone":1, "emerald_block":1, "stained_hardened_clay": -1000, "redstone_block": 1000}
+        BLOCK_TYPES = {"stone":-1000, "glowstone":0, "emerald_block":0, "stained_hardened_clay": -1000, "redstone_block": 1000}
+        alpha = 0.9
         gamma = 0.8
+        epsilon = 0.1
         noise = 0.0
-
-        if True:
-            print Q
-            state_t = self.agent_host.getWorldState()
-            while state_t.is_mission_running:
-                if state_t.number_of_observations_since_last_state > 0:  # Has any Oracle-like and/or internal sensor observations come in?
-                    msg = state_t.observations[-1].text  # Get the detailed for the last observed state
-                    oracle = json.loads(msg)  # Parse the Oracle JSON
-
-                    # Oracle
-                    grid = oracle.get(u'grid', 0)
-
-                    # GPS-like sensor
-                    xpos = oracle.get(u'XPos', 0)  # Position in 2D plane, 1st axis
-                    zpos = oracle.get(u'ZPos', 0)  # Position in 2D plane, 2nd axis (yes Z!)
-                    ypos = oracle.get(u'YPos', 0)
-
-                    x = int(xpos)
-                    z = int(zpos)
-                    curr_s = 10*z + x
-                    print "\nCurrent position: " + str(x) + " " + str(z)
-
-                    highest_Q_idx = np.argmax(Q[curr_s])
-                    curr_a = self.__ExecuteActionForRealisticAgentWithNoisyTransitionModel__(highest_Q_idx, noise)
-                    time.sleep(1)
-
-                state_t = self.agent_host.getWorldState()
-            return
+        reward_cumulative = 0.0
 
         state_t = self.agent_host.getWorldState()
 
@@ -119,15 +95,34 @@ class AgentRealistic:
                 curr_s = 10*z + x
                 print "\nCurrent position: " + str(x) + " " + str(z)
 
-                allowed_actions_idx = [] # between 0 and 3
                 for i in range(len(grid)):
-                    block = BLOCK_TYPES[str(grid[i])]
-                    if i%2 != 0 and block >= 0:
-                        allowed_actions_idx.append((i-1)/2)
+                    if i%2 != 0 and BLOCK_TYPES[str(grid[i])] < 0:
+                        Q[curr_s][(i-1)/2] = -100
 
+                rnd = random.random()
+                curr_a_idx = None
+                allowed_actions_idx = [] # between 0 and 3
+                if rnd < epsilon: # take random action
+                    for i in range(len(grid)):
+                        if i%2 != 0 and BLOCK_TYPES[str(grid[i])] >= 0:
+                            allowed_actions_idx.append((i-1)/2)
+                else: # choose action based on Q table values
+                    max_val = max(Q[curr_s])
+                    for i in range(len(Q[curr_s])):
+                        if Q[curr_s][i] == max_val and BLOCK_TYPES[str(grid[i*2 + 1])] >= 0:
+                            allowed_actions_idx.append(i)
+
+                print allowed_actions_idx
                 curr_a_idx = random.choice(allowed_actions_idx)
                 curr_a = self.__ExecuteActionForRealisticAgentWithNoisyTransitionModel__(curr_a_idx, noise)
-                r = BLOCK_TYPES[str(grid[curr_a_idx*2+1])]
+
+                r = sum(r.getValue() for r in state_t.rewards)
+                if (r < 0):
+                    r = 0
+                reward_cumulative += r
+                self.solution_report.addReward(r, datetime.datetime.now())
+                print("Reward:", r)
+                print("Cummulative reward so far:", reward_cumulative)
 
                 next_x = x
                 next_z = z
@@ -141,11 +136,11 @@ class AgentRealistic:
                     next_z += 1
 
                 next_s = 10*next_z + next_x
-                Q[curr_s][curr_a_idx] = r + gamma*(max(Q[next_s]))
-
-                print Q
+                Q[curr_s][curr_a_idx] = (1-alpha)*Q[curr_s][curr_a_idx] +\
+                    alpha*(r + gamma*(max(Q[next_s])))
 
             state_t = self.agent_host.getWorldState()
+            time.sleep(0.2)
 
-        pickle.dump(Q, open("Q.txt", "wb"))
+        pickle.dump(Q, open("Q.pickle", "wb"))
         return
